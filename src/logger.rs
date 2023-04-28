1ad9ee39b5eafef006aff3f2c2b1f82bbc49e542
pwh-pwh <plmoknpwh@163.com>
2023-04-28 17:20:25 +0800

    add logger.rs

---
+++ src/logger.rs
@@ -0,0 +1,32 @@
+use std::{env, fs};
+use log::{debug, error, info, trace, warn};
+
+pub fn init() ->Result<(),fern::InitError> {
+    let log_level = env::var("LOG_LEVEL").unwrap_or("INFO".into());
+    let log_level = log_level.parse::<log::LevelFilter>()
+        .unwrap_or(log::LevelFilter::Info);
+    let mut builder = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "[{}][{}][{}] {}",
+                chrono::Local::now().format("%H:%M:%S"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log_level)
+        // log to stderr
+        .chain(std::io::stderr());
+    if let Ok(log_file) = env::var("LOG_FILE") {
+        let log_file = fs::File::create(log_file)?;
+        builder = builder.chain(log_file);
+    }
+    builder.apply()?;
+    trace!("TRACE output enabled");
+    debug!("DEBUG output enabled");
+    info!("INFO output enabled");
+    warn!("WARN output enabled");
+    error!("ERROR output enabled");
+    Ok(())
+}
